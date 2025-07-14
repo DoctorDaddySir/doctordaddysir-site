dc64ea4e9a95f2b150b4ef79dd15634f2a1e1c53
Doctor Daddy Sir <perry.t.shelton@gmail.com>
2025-07-14 13:04:38 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#A64AC9",
+        background: "#0D0D0D",
+        accent: "#6A1B9A",
+      },
+    },
+  },
+  plugins: [],
+};
